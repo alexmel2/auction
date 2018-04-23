dee7f666d8fa02bf44cd167c134dc6b5c16c123d
LAPTOPPC\U <melnik@g.jct.ac.il>
2018-04-23 17:16:40 +0300

    hh

---
+++ app/components/application/application.ts
@@ -0,0 +1,20 @@
+import { Component, ViewEncapsulation } from "@angular/core";
+import { ProductService, Product } from "../../services/product-service";
+
+
+
+ @Component({
+    selector: 'auction-application', 
+    templateUrl: 'app/components/application/application.html',
+    styleUrls: ['app/components/application/application.css'], 
+    encapsulation:ViewEncapsulation.None
+  })
+
+export default class ApplicationComponent
+{
+  products:Array<Product>=[];
+constructor (private productService:ProductService){
+  this.products=this.productService.getProducts();
+}
+
+}
---
+++ app/components/stars/stars.ts
@@ -0,0 +1,18 @@
+import { OnInit, Input, Component } from "@angular/core";
+@Component({
+    templateUrl: 'app/components/stars/stars.html',
+    styles: [` .starrating { color: #d17581; }`],
+    selector: 'auction-stars'
+  })
+export default class StarsComponent implements OnInit {
+    ngOnInit(): void {
+        for (let i = 1; i <= this.count; i++) {
+            this.stars.push(i > this.rating);
+        }
+    }
+    @Input() count: number = 5;
+    @Input() rating: number = 0;
+    stars: boolean[] = [];
+
+
+}
