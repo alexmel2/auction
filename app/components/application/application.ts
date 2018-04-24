7dacc009194e5ee25de7b459f084c0d603d1aadf
LAPTOPPC\U <melnik@g.jct.ac.il>
2018-04-25 00:12:54 +0300

    החלפת depandance

--- app/components/application/application.ts
+++ app/components/application/application.ts
@@ -1,20 +1,9 @@
-import { Component, ViewEncapsulation } from "@angular/core";
-import { ProductService, Product } from "../../services/product-service";
+import {Component, ViewEncapsulation} from '@angular/core';
 
-
-
- @Component({
-    selector: 'auction-application', 
-    templateUrl: 'app/components/application/application.html',
-    styleUrls: ['app/components/application/application.css'], 
-    encapsulation:ViewEncapsulation.None
-  })
-
-export default class ApplicationComponent
-{
-  products:Array<Product>=[];
-constructor (private productService:ProductService){
-  this.products=this.productService.getProducts();
-}
-
-}
+@Component({
+  selector: 'auction-application',
+  templateUrl: 'app/components/application/application.html',
+  styleUrls: ['app/components/application/application.css'],
+  encapsulation:ViewEncapsulation.None
+})
+export default class ApplicationComponent {}
--- app/components/stars/stars.ts
+++ app/components/stars/stars.ts
@@ -1,18 +1,22 @@
-import { OnInit, Input, Component } from "@angular/core";
+// tag::annotations[]
+import {Component, OnInit} from '@angular/core';
+
 @Component({
-    templateUrl: 'app/components/stars/stars.html',
-    styles: [` .starrating { color: #d17581; }`],
-    selector: 'auction-stars'
-  })
+  templateUrl: 'app/components/stars/stars.html',
+  selector: 'auction-stars',
+  inputs: ['rating', 'count'] // <1>
+})
+// end::annotations[]
+// tag::class[]
 export default class StarsComponent implements OnInit {
-    ngOnInit(): void {
-        for (let i = 1; i <= this.count; i++) {
-            this.stars.push(i > this.rating);
-        }
+  count: number = 5; // <1>
+  rating: number = 0; // <2>
+  stars: boolean[] = []; // <3>
+
+  ngOnInit() { // <4>
+    for (let i = 1; i <= this.count; i++) {
+      this.stars.push(i > this.rating);
     }
-    @Input() count: number = 5;
-    @Input() rating: number = 0;
-    stars: boolean[] = [];
-
-
+  }
 }
+// end::class[]
